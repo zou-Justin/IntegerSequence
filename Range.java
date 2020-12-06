d40853e7fadfb27dfddbd29766d55c4c0fbb6a8f
Justin Dylan <justin82049@gmail.com>
2020-12-05 22:07:25 -0500

    added interface and basic range from class notes

---
+++ Range.java
@@ -0,0 +1,20 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+  private int start,end,current;
+
+  //@param start : the starting value (inclusive) which must be <= end.
+  //@param end : the ending value which is also inclusive.
+  public Range(int start,  int end){  }
+
+  public void reset(){  }
+  public int length(){  }
+
+  //When current is no longer a valid element in the range, it should return false.
+  public boolean hasNext(){  }
+
+  //@throws NoSuchElementException when hasNext() is false.
+  //This will return the current value, it will also increase current value by 1.
+  //e.g.  if current is 5. This will make current 6, and return 5.
+  public int next(){    }
+
+}
