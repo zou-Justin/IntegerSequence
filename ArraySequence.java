0712b0c66aab86ed0175272c76d4ee4162028074
Justin Dylan <justin82049@gmail.com>
2020-12-07 12:47:41 -0500

    filled in the methods from interface

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,25 +1,35 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   int currentIndex;
-  int []data;
+  int data[];
+
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
     for (int i : other){
       data[i] = other[i];
     }
+    currentIndex = 0;
   }
   public boolean hasNext(){
-
+    if (currentIndex == data.length + 1){
+      return false;
+    }
+    return true;
   }
   public int next(){
-
+    if (hasNext() == false){
+      throw new NoSuchElementException("bad");
+    }
+    int temp = currentIndex;
+    currentIndex++;
+    return data[temp];
   }
   public int length(){
-
+    return data.length;
   }
   public void reset(){
-    
+    currentIndex = 0;
   }
 
 
