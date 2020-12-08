cccc4d1a78717bf866f50da451cafd67d52a6464
Justin Dylan <justin82049@gmail.com>
2020-12-08 17:46:08 -0500

    fixed nullpointerexception

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,29 +1,30 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
-  int currentIndex;
-  int data[];
-
+  private int currentIndex;
+  private int [] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
-    for (int i : other){
-      data[i] = other[i];
-    }
+    data = new int[other.length];
+    for (int i = 0; i < other.length;i++)
+    {
+       data[i] = other[i];
+     }
     currentIndex = 0;
   }
   public boolean hasNext(){
-    if (currentIndex == data.length + 1){
-      return false;
-    }
-    return true;
+    return (currentIndex > data.length);
   }
   public int next(){
     if (hasNext() == false){
       throw new NoSuchElementException("bad");
     }
+    else{
     int temp = currentIndex;
     currentIndex++;
     return data[temp];
+    }
+
   }
   public int length(){
     return data.length;
